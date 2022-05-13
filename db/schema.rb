28ea380ccb08918ce8ccc23e7e04db181798df73
Rkanraeden <lecortezc@gmail.com>
2022-05-13 19:13:49 -0400

    third commit 20220513

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_13_225947) do
+ActiveRecord::Schema.define(version: 2022_05_13_231217) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
