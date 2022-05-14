549a6441151846f19c8bd277982c54ca61179393
Rkanraeden <lecortezc@gmail.com>
2022-05-13 20:53:29 -0400

    fourth commit 20220513

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: %i[ show edit update destroy ]
+
     def index
         @posts = Post.all
     end
@@ -12,11 +14,16 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(params)
-        if @post.save
-            redirect_to @post, notice: "Post was successfully created."
-        else
-            render :new, status: :unprocessable_entity
+        @post = Post.new(post_params)
+       
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
         end
     end
 
@@ -33,7 +40,19 @@ class PostsController < ApplicationController
         @post.destroy
         redirect_to posts_url, notice: "Post was successfully destroyed."
     end
+    
+    private
+           
+        # Use callbacks to share common setup or constraints between actions.
+        def set_post
+            @post = Post.find(params[:id])
+        end
+    
+        # Only allow a list of trusted parameters through.
+        def post_params
+            params.require(:post).permit(:title, :description, :author)
+        end
+    
         
-
-        
+      
 end     
